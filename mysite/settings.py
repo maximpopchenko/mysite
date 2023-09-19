9323a0226298a8a3917e24d792d04a588b03b6b0
maximpopchenko <maximpopchenko@gmail.com>
2023-09-19 18:22:32 +0300

    Добавлена система комментариев к постам

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,7 +127,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # Конфигурация сервера электронной почты
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'maximpopchenko@gmail.com'
-EMAIL_HOST_PASSWORD = 'oarpknoucnqqzvqb'
+EMAIL_HOST_PASSWORD = '...'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # писать электронные письма в консоль
