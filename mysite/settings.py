66c2b9678da82943518458fd8a7854edd65a7218
maximpopchenko <maximpopchenko@gmail.com>
2023-09-21 17:19:18 +0300

    Переключился на PostgreSQL

--- mysite/settings.py
+++ mysite/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -75,17 +74,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "mysite.wsgi.application"
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blog',
+        'USER': 'blog',
+        'PASSWORD': 'blog',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -105,7 +104,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -116,7 +114,6 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -134,5 +131,4 @@ EMAIL_HOST_USER = 'maximpopchenko@gmail.com'
 EMAIL_HOST_PASSWORD = '...'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # писать электронные письма в консоль
-
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  # писать электронные письма в консоль
